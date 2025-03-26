8a94c8b94a560b61d2d786b6e93365b4a69e46b1
ERF88 <erf_3d@hotmail.com>
2025-03-26 12:20:04 -0300

    criando estrutura inicial do projeto

---
+++ orchestrator-service/src/main/java/com/github/erf88/orchestrated/orchestratorservice/OrchestratorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.erf88.orchestrated.orchestratorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchestratorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrchestratorServiceApplication.class, args);
+	}
+
+}
---
+++ product-validation-service/src/main/java/com/github/erf88/orchestrated/productvalidationservice/ProductValidationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.erf88.orchestrated.productvalidationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductValidationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductValidationServiceApplication.class, args);
+	}
+
+}
